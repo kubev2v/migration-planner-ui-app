825a2797180d73ab0b264f2a7bf334cc63f67f73
Montse Ortega <mortegag@redhat.com>
2025-04-11 14:32:12 +0200

    Solving problems with token
    Signed-off-by: Montse Ortega <mortegag@redhat.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useMemo } from "react";
 import { useChrome } from "@redhat-cloud-services/frontend-components/useChrome";
 import { Spinner } from "@patternfly/react-core";
-import {
-  Container  
-} from "@migration-planner-ui/ioc";
+import { Container } from "@migration-planner-ui/ioc";
 import { Provider as DependencyInjectionProvider } from "@migration-planner-ui/ioc";
 import { Configuration } from "@migration-planner-ui/api-client/runtime";
 import { AgentApi } from "@migration-planner-ui/agent-client/apis";
+import { ImageApi, SourceApi } from "@migration-planner-ui/api-client/apis";
 import Routing from "./Routing";
 import { Symbols } from "./main/Symbols";
-import { ImageApi, SourceApi } from "@migration-planner-ui/api-client/apis";
 import { useAccountsAccessToken } from "./hooks/useAccountsAccessToken";
 
 function getConfiguredContainer(accessToken: string): Container {
   const plannerApiConfig = new Configuration({
     basePath: "/planner",
     headers: {
-      Authorization: `Bearer ${accessToken}`, // Use access token here
+      Authorization: `Bearer ${accessToken}`,
     },
   });
 
   const container = new Container();
-  
+
   container.register(Symbols.ImageApi, new ImageApi(plannerApiConfig));
   container.register(Symbols.SourceApi, new SourceApi(plannerApiConfig));
   container.register(Symbols.AgentApi, new AgentApi(plannerApiConfig));
@@ -36,12 +34,21 @@ function getConfiguredContainer(accessToken: string): Container {
 const App = () => {
   const { updateDocumentTitle } = useChrome();
   const { accessToken } = useAccountsAccessToken();
- 
+
   useEffect(() => {
     updateDocumentTitle("Migration assessment");
   }, []);
 
-  const container = getConfiguredContainer(accessToken);
+  // Reconfigure the container only when the token changes
+  const container = useMemo(() => {
+    if (!accessToken) return undefined;
+    return getConfiguredContainer(accessToken);
+  }, [accessToken]);
+
+  // While we wait for the token, we show loading
+  if (!container) {
+    return <Spinner />;
+  }
 
   return (
     <Fragment>
